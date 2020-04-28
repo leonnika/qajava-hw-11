d28be048308d8871dfeaaa6aed2f09408f576f59
Ekaterina Rymareva <rymareva_ea@mail.ru>
2020-04-29 01:02:07 +0500

    changed metod matches added test for repository

--- src/main/java/ru/netology/repository/ProductRepository.java
+++ src/main/java/ru/netology/repository/ProductRepository.java
@@ -18,17 +18,17 @@ public class ProductRepository {
         return product;
     }
 
-   public void removeById(int id) {
-       int length = product.length - 1;
-       Product[] tmp = new Product[length];
-       int index = 0;
-       for (Product item : product) {
-           if (item.getId() != id) {
-               tmp[index] = item;
-               index++;
-           }
-       } // меняем наши элементы items = tmp;
-       product = tmp;
+    public void removeById(int id) {
+        int length = product.length - 1;
+        Product[] tmp = new Product[length];
+        int index = 0;
+        for (Product item : product) {
+            if (item.getId() != id) {
+                tmp[index] = item;
+                index++;
+            }
+        } // меняем наши элементы items = tmp;
+        product = tmp;
 
     }
 }
