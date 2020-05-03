a238ec56bf3b5e412a174fbc9677968fc37b5232
Ekaterina Rymareva <rymareva_ea@mail.ru>
2020-05-03 16:25:21 +0500

    added init in tests

--- src/test/java/ru/netology/manager/ProductManagerBookTest.java
+++ src/test/java/ru/netology/manager/ProductManagerBookTest.java
@@ -9,37 +9,30 @@ import ru.netology.repository.ProductRepository;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class ProductManagerBookTest {
-
-    private Book firstBook= new Book(1, "Война и мир.том1", 1000, "Л.Н.Толстой");
-    private Book secondBook= new Book(2, "Война и мир.том2", 1000, "Л.Н.Толстой");
+    ProductRepository repository = new ProductRepository();
+    ProductManager manager = new ProductManager(repository);
+    private Book firstBook = new Book(1, "Война и мир.том1", 1000, "Л.Н.Толстой");
+    private Book secondBook = new Book(2, "Война и мир.том2", 1000, "Л.Н.Толстой");
     private Book thirdBook = new Book(3, "Война и мир.том3", 1000, "Л.Н.Толстой");
-    private Book fourthBook= new Book(4, "Война и мир.том4", 1000, "Л.Н.Толстой");
+    private Book fourthBook = new Book(4, "Война и мир.том4", 1000, "Л.Н.Толстой");
 
     @BeforeEach
     void init() {
+        manager.addPoduct(firstBook);
+        manager.addPoduct(secondBook);
+        manager.addPoduct(thirdBook);
+        manager.addPoduct(fourthBook);
     }
 
     @Test
     void shouldSearcyByBookAutor() {
-        ProductRepository repository = new ProductRepository();
-        ProductManager manager = new ProductManager(repository);
-        manager.AddPoduct(firstBook);
-        manager.AddPoduct(secondBook);
-        manager.AddPoduct(thirdBook);
-        manager.AddPoduct(fourthBook);
-       Product[] actual = manager.searcyBy("Л.Н.Толстой");
-        Product[] expected = new Book[]{firstBook,secondBook,thirdBook,fourthBook};
+        Product[] actual = manager.searcyBy("Л.Н.Толстой");
+        Product[] expected = new Book[]{firstBook, secondBook, thirdBook, fourthBook};
         assertArrayEquals(expected, actual);
     }
 
     @Test
     void shouldSearcyByBookName() {
-        ProductRepository repository = new ProductRepository();
-        ProductManager manager = new ProductManager(repository);
-        manager.AddPoduct(firstBook);
-        manager.AddPoduct(secondBook);
-        manager.AddPoduct(thirdBook);
-        manager.AddPoduct(fourthBook);
         Product[] actual = manager.searcyBy("Война и мир.том1");
         Product[] expected = new Book[]{firstBook};
         assertArrayEquals(expected, actual);
@@ -47,12 +40,6 @@ class ProductManagerBookTest {
 
     @Test
     void shouldSearcyByBookNoСoincidence() {
-        ProductRepository repository = new ProductRepository();
-        ProductManager manager = new ProductManager(repository);
-        manager.AddPoduct(firstBook);
-        manager.AddPoduct(secondBook);
-        manager.AddPoduct(thirdBook);
-        manager.AddPoduct(fourthBook);
         manager.remById(1);
         Product[] actual = manager.searcyBy("Отцы и дети");
         Product[] expected = new Book[]{};
--- src/test/java/ru/netology/manager/ProductManagerSmartphoneTest.java
+++ src/test/java/ru/netology/manager/ProductManagerSmartphoneTest.java
@@ -1,27 +1,31 @@
 package ru.netology.manager;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import ru.netology.domain.Smartphone;
 import ru.netology.domain.Product;
+import ru.netology.domain.Smartphone;
 import ru.netology.repository.ProductRepository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class ProductManagerSmartphoneTest {
-    private Smartphone firstSmartphone = new Smartphone(1,"iPhone8.0", 20000, "Apple");
-    private Smartphone secondSmartphone= new Smartphone(2, "iPhone SE", 40000, "Apple");
-    private Smartphone thirdSmartphone= new Smartphone(3, "iPhone 11", 100000, "Apple");
+    ProductRepository repository = new ProductRepository();
+    ProductManager manager = new ProductManager(repository);
+    private Smartphone firstSmartphone = new Smartphone(1, "iPhone8.0", 20000, "Apple");
+    private Smartphone secondSmartphone = new Smartphone(2, "iPhone SE", 40000, "Apple");
+    private Smartphone thirdSmartphone = new Smartphone(3, "iPhone 11", 100000, "Apple");
     private Smartphone fourthSmartphone = new Smartphone(4, "Samsung Galaxy A50", 25000, "Samsung");
 
+    @BeforeEach
+    void init() {
+        manager.addPoduct(firstSmartphone);
+        manager.addPoduct(secondSmartphone);
+        manager.addPoduct(thirdSmartphone);
+        manager.addPoduct(fourthSmartphone);
+    }
 
     @Test
     void searcyBySmartphoneName() {
-        ProductRepository repository = new ProductRepository();
-        ProductManager manager = new ProductManager(repository);
-        manager.AddPoduct(firstSmartphone);
-        manager.AddPoduct(secondSmartphone);
-        manager.AddPoduct(thirdSmartphone);
-        manager.AddPoduct(fourthSmartphone);
         Product[] actual = manager.searcyBy("Samsung Galaxy A50");
         Product[] expected = new Smartphone[]{fourthSmartphone};
         assertArrayEquals(expected, actual);
@@ -29,25 +33,13 @@ class ProductManagerSmartphoneTest {
 
     @Test
     void searcyBySmartphoneProducer() {
-        ProductRepository repository = new ProductRepository();
-        ProductManager manager = new ProductManager(repository);
-        manager.AddPoduct(firstSmartphone);
-        manager.AddPoduct(secondSmartphone);
-        manager.AddPoduct(thirdSmartphone);
-        manager.AddPoduct(fourthSmartphone);
         Product[] actual = manager.searcyBy("Apple");
-        Product[] expected = new Smartphone[]{firstSmartphone,secondSmartphone,thirdSmartphone};
+        Product[] expected = new Smartphone[]{firstSmartphone, secondSmartphone, thirdSmartphone};
         assertArrayEquals(expected, actual);
     }
 
     @Test
     void searcyBySmartphoneNoСoincidence() {
-        ProductRepository repository = new ProductRepository();
-        ProductManager manager = new ProductManager(repository);
-        manager.AddPoduct(firstSmartphone);
-        manager.AddPoduct(secondSmartphone);
-        manager.AddPoduct(thirdSmartphone);
-        manager.AddPoduct(fourthSmartphone);
         Product[] actual = manager.searcyBy("Nokia");
         Product[] expected = new Smartphone[]{};
         assertArrayEquals(expected, actual);
